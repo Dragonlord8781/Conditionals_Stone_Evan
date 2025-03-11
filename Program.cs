5a1b72da6d5278995fe0e62fa15a8767578fda5b
Dragonlord8781 <evan.stone638@gmail.com>
2025-03-11 09:29:16 -0500

    update

--- Program.cs
+++ Program.cs
@@ -6,17 +6,47 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a whole number");
-            int firstNum = Console.ReadLine();
+            //Ask for a whole number
+            Console.WriteLine("Please enter a whole number, and we'll see if its postive or negative, even or odd");
+            //declares firstNum and assigns it the inputted whole number
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            //declares posNeg and have it determine true if firstNum is greater than 0 and false if firstNum is less than 0
             bool posNeg = firstNum > 0;
+            //if posNeg is true writes a line stating that firstNum is a postive number
             if (posNeg == true)
             {
-                Console.WriteLine($"{firstNum} is a postive number");
+                Console.WriteLine($"{firstNum} is postive");
             }
+            //otherwise if posNeg is false it writes a line stating that firstNum is a negative number
             else if (posNeg == false) 
             {
-                Console.WriteLine($"{firstNum} is a negative number");
+                Console.WriteLine($"{firstNum} is negative");
             }
+            //writes a line saying "and" to both seperate the two if-else sections and to connect them as effecting the same input
+            Console.WriteLine("and");
+            //detemines if firstNum is % 2 == 0 then write a statement saying its even, otherwise write a statement saying its odd
+            if (firstNum % 2 == 0) 
+            {
+                Console.WriteLine($"{firstNum} is even");
+            }
+            else
+            { 
+                Console.WriteLine($"{firstNum} is odd");
+            }
+            //asks for a second whole number
+            Console.WriteLine("Please enter a second whole number");
+            //declares secNum and assigns the inputted number
+            int secNum = Convert.ToInt32(Console.ReadLine());
+            //asks for a third whole number
+            Console.WriteLine("Now please enter a third whole number");
+            //declares triNum and assigns the inputted number
+            int triNum = Convert.ToInt32(Console.ReadLine());
+            //states its comparing the three numbers
+            Console.WriteLine("Comparing...");
+            //declares largest and assignes the largest int out of firstNum, secNum, and triNum by compairing firstNum to the larger int of secNum or triNum
+            int largest = Math.Max(firstNum, Math.Max(secNum, triNum));
+            //writes a line saying largest is the largest number
+            Console.WriteLine($"{largest} is the largest number");
         }
     }
 }
